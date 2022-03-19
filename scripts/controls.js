d85e83950f8ae88aa1401d3c78ba704d60f3f746
Wilbur Acharya <wacharya@uno.edu>
2022-03-19 12:04:22 -0500

    Modified Controls

--- scripts/controls.js
+++ scripts/controls.js
@@ -30,26 +30,4 @@ function movePaddle() {
     }
 }
 
-function play() {
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball.draw();
-    paddle.draw();
-    movePaddle();
-
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-
-    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-        ball.dx *= -1;
-
-    }
-
-    if (ball.y + ball.radius > canvas.width || ball.y - ball.radius < 0) {
-        ball.dy *= -1;
-
-    }
-
-    requestAnimationFrame(play);
-}
-play();
